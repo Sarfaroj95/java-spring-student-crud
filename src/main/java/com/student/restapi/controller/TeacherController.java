77ab0a291f792431029bbaf97b8b164114ed5846
sreedipto <sreedipto6@gmail.com>
2025-12-07 01:20:37 +0530

    changes teacher controller

--- src/main/java/com/student/restapi/controller/TeacherController.java
+++ src/main/java/com/student/restapi/controller/TeacherController.java
@@ -2,8 +2,11 @@ package com.student.restapi.controller;
 
 import java.util.List;
 
+import com.student.restapi.entity.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.student.restapi.entity.Teacher;
@@ -21,5 +24,12 @@ public class TeacherController {
 		return teacher;
 		
 	}
+
+
+    @PostMapping("/teachers/add")
+    public void createTeacher(@RequestBody Teacher teacher) {
+
+        repo.save(teacher);
+    }
 
 }
