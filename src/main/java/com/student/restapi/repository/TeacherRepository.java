25ba8bd95cba08baf26395d6e05d177481dfcd84
sarfaroj <sarfaroj95@gmail.com>
2025-12-07 02:12:31 +0530

    Modify the Student to Employee control and repo

--- src/main/java/com/student/restapi/repository/TeacherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.student.restapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.student.restapi.entity.Teacher;
-
-public interface TeacherRepository extends JpaRepository<Teacher, Integer>  {
-
-}
