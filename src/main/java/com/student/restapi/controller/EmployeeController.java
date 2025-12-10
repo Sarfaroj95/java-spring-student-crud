cd06173aa8844d8f061e8329bcb983104cb37a98
sarfaroj <sarfaroj95@gmail.com>
2025-12-11 02:12:33 +0530

    Create Employee Service ans use in control

--- src/main/java/com/student/restapi/controller/EmployeeController.java
+++ src/main/java/com/student/restapi/controller/EmployeeController.java
@@ -11,23 +11,51 @@ import java.util.List;
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
-	
+
+
+	public final EmployeeService employeeService;
+
 	@Autowired
-	EmployeeRepository repo;
-	EmployeeService service;
+	private EmployeeRepository repo;
+
+	@Autowired
+	public EmployeeController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
+	}
+
 	
 	@GetMapping
 	public List<Employee> getAllEmployees(){
-		List<Employee> employeeList = repo.findAll();
-		return employeeList;
+		return employeeService.employeeRepo.findAll();
+	}
 
+	@GetMapping("/{id}")
+	public Employee getEmployee(@PathVariable int id) {
+		return employeeService.getEmployeeById(id);
 	}
 
 
     @PostMapping("/add")
     public void createEmployee(@RequestBody Employee employee) {
-
-        repo.save(employee);
+        employeeService.createEmployee(employee);
     }
+
+	@PutMapping("/update/{id}")
+		public void updateEmployee(@PathVariable int id, @RequestBody Employee updateEmployee) {
+			Employee employee = repo.findById(id).get();
+			employee.setEmployee_name(updateEmployee.getEmployee_name());
+			employee.setEmployee_id(updateEmployee.getEmployee_id());
+			employee.setEmployee_joined_date(updateEmployee.getEmployee_joined_date());
+			employee.setEmployee_age(updateEmployee.getEmployee_age());
+			employee.setEmployee_designation(updateEmployee.getEmployee_designation());
+			employee.setEmployee_deprt(updateEmployee.getEmployee_deprt());
+			employeeService.updateEmployee(employee);
+		}
+
+	@DeleteMapping("/{id}")
+	public void deleteEmployee(@PathVariable int id) {
+		employeeService.deleteEmployee(id);
+	}
+
 
 }
--- src/main/java/com/student/restapi/service/EmployeeService.java
+++ src/main/java/com/student/restapi/service/EmployeeService.java
@@ -1,8 +1,12 @@
 package com.student.restapi.service;
 
+import com.student.restapi.entity.Employee;
 import com.student.restapi.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class EmployeeService {
     public final EmployeeRepository employeeRepo;
 
@@ -11,11 +15,18 @@ public class EmployeeService {
         this.employeeRepo = employeeRepo;
     }
 
+    public void createEmployee(Employee employee) {
+        employeeRepo.save(employee);
+    }
    public void getAllEmployees() {
         employeeRepo.findAll();
     }
 
-    public void updateEmployee(com.student.restapi.entity.Employee employee) {
+    public Employee getEmployeeById(int id) {
+        return employeeRepo.findById(id).orElse(null);
+    }
+
+    public void updateEmployee(Employee employee) {
         employeeRepo.save(employee);
     }
 
